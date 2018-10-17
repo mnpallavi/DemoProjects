384e06920db88ce169a291f3edcdfc1911640937
mnpallavi <mnpallavi@dresource.com>
2018-10-17 16:07:52 +0530

    adding new files

---
+++ Student1.rb
@@ -0,0 +1,26 @@
+Class Student1 {
+
+ def initialize(name ="world")
+   			puts ("my name" +name) 
+   	 end
+
+   
+   def student
+   	 
+   puts ("student name")
+ 
+end
+   
+  def parents
+  	
+ puts ("parents details")
+  
+end
+end
+}
+   
+begin
+e = Student1.new("pallavi")
+e.student
+e.parents
+e.halting;
---
+++ time.rb
@@ -0,0 +1,9 @@
+time1 = Time.new
+puts "Current Time : " + time1.inspect
+
+# Time.now is a synonym:
+time2 = Time.now
+puts "Current Time : " + time2.inspect
+
+time3=Time.new
+puts time3.year, time3.month, time3.day
